1ebadc685a31d978f8dcb23ec72cc44cf53db2f2
agitoz <rteja764@gmail.com>
2024-01-08 10:02:14 +0800

    updated setup

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='PyMask',
     version='0.3',
+    description='Dead simple image annotation tool to create binary mask via command line',
+    author='Amitoz Azad',
+    url='https://github.com/aGIToz/PyMask',
+    license='MIT',
     packages=find_packages(),
     install_requires=[
         'opencv-python',
